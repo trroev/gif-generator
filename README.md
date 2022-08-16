3595e644b178e7dab4b06701c49e5c7071caec7b
Trevor Mathiak <tmathiak@gmail.com>
2022-08-16 13:47:59 -0400

    finished product. Refactored JS, updated styling, updated README

--- README.md
+++ README.md
@@ -9,9 +9,12 @@ A simple gif generator that uses the Giphy API to randomly generate a gif
 
 **Tech used:** HTML, CSS, JavaScript
 
-
+Fun little project that was a good introduction to working with APIs
 
 ## Optimizations
 
+Was able to hack together some code that worked but I wasn't happy with it's spaghetti-ness. Refactored the JavaScript to make it much more readable and optimal by getting rid of global variables and making reusable functions.
 
 ## Lessons Learned:
+
+My first foray into asynchronous JavaScript and utilizing APIs. I can definitely see how necessary these things will be in in my future coding career.
--- script.js
+++ script.js
@@ -1,25 +1,40 @@
-const img = document.querySelector("img");
-const randomBtn = document.querySelector(".random");
-const searchBtn = document.querySelector(".search-button");
+const fetchGiphy = (query) => {
+  const url =
+    "https://api.giphy.com/v1/gifs/translate?api_key=QmEZnA0ikkQ9kaMMYQWPOgt4xBDtdK5P&s=";
+  const img = document.querySelector("img");
 
-const fetchCats = () => {
-  fetch(
-    "https://api.giphy.com/v1/gifs/translate?api_key=QmEZnA0ikkQ9kaMMYQWPOgt4xBDtdK5P&s=nic-cage-AND-nicolas-cage",
-    { mode: "cors" }
-  )
+  fetch(url + query, { mode: "cors" })
     .then(function (response) {
       return response.json();
     })
     .then(function (response) {
       img.src = response.data.images.original.url;
     })
-    .catch((err) => {
-      console.log("There was an error with the fetch request");
+    .catch((error) => {
+      console.error("There was an error with the fetch request", error);
     });
 };
 
-fetchCats();
+fetchGiphy("nic-cage-AND-nicolas-cage");
 
-randomBtn.addEventListener("click", (e) => {
-  fetchCats();
-});
+const handleButtons = (() => {
+  const randomBtn = document.querySelector(".random");
+  const searchBtn = document.querySelector(".search-button");
+  const input = document.querySelector("input");
+
+  randomBtn.addEventListener("click", () => {
+    fetchGiphy("nic-cage-AND-nicolas-cage");
+  });
+
+  searchBtn.addEventListener("click", () => {
+    const searchInput = document.querySelector("#gif-search").value.split(" ").join("+").toLowerCase();
+
+    fetchGiphy(searchInput);
+  })
+
+  input.addEventListener("keypress", (e) => {
+    if(e.keyCode == 13) {
+      searchBtn.click();
+    }
+  })
+})();
