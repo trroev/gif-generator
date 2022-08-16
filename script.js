0426a4310ae03339240334cbf64013b1e189b797
Trevor Mathiak <tmathiak@gmail.com>
2022-08-16 14:43:25 -0400

    refactored fetch to us async/await

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-const fetchGiphy = (query) => {
+const fetchGiphy = async (query) => {
   const url =
     "https://api.giphy.com/v1/gifs/translate?api_key=QmEZnA0ikkQ9kaMMYQWPOgt4xBDtdK5P&s=";
   const img = document.querySelector("img");
 
-  fetch(url + query, { mode: "cors" })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      img.src = response.data.images.original.url;
-    })
-    .catch((error) => {
+  try {
+  const response = await fetch(url + query, { mode: "cors" });
+  const gifData = await response.json();
+  img.src = gifData.data.images.original.url;
+  } catch (error) {
       console.error("There was an error with the fetch request", error);
-    });
+    };
 };
 
 fetchGiphy("nic-cage-AND-nicolas-cage");
